3388e7fe906a35e1b56c5c275719f450e90525cd
biskyt <biskyt@users.noreply.github.com>
2018-09-06 13:22:28 +0100

    Optimisations for build caching

--- base-apache/set-sources.sh
+++ base-apache/set-sources.sh
@@ -14,5 +14,4 @@ touch /etc/apt/sources.list.d/openeyes-dependencies.list
 sed "s|\$OS_VERSION|$OS_VERSION|" sources/sources | tee /etc/apt/sources.list.d/openeyes-dependencies.list
 chmod 755 /etc/apt/sources.list.d/openeyes-dependencies.list
 
-apt-get update
-
+# DO NOT apt-get update here or you risk creating a new layer - call with apt-get install RUN statement
