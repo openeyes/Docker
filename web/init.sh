d26b2f02c6a643fe15eb7b65cbe84d0f095bd3ec
biskyt <biskyt@users.noreply.github.com>
2019-01-20 22:11:49 +0000

    Installs - still some tweks required

--- web/init.sh
+++ web/init.sh
@@ -36,49 +36,56 @@ idfilecontent="Host github.com\nStrictHostKeyChecking no"
 [ -f /run/secrets/ssh_key ] && { cp /run/secrets/ssh_key /root/.ssh/id_rsa; echo -e "$idfilecontent\nIdentityFile ~/.ssh/id_rsa" > /root/.ssh/config ; }
 chmod 600 /root/.ssh/*
 
-# If no web files exist, check them out locally
-if [ ! -d /openeyes/protected ]; then
-  ssh git@github.com -T
-  [ "$?" == "1" ] && cloneroot="git@github.com:" || cloneroot="https://"
-  [ -z "$GIT_ORG" ] && { [ "$cloneroot" == "https://" ] && gitroot="appertafoundation" || gitroot="openeyes";} || gitroot=$GIT_ORG
-echo cloning "-b ${BUILD_BRANCH} $cloneroot${gitroot}/openeyes.git"
-  git clone --depth=1 -b ${BUILD_BRANCH} $cloneroot${gitroot}/openeyes.git /openeyes
-  /openeyes/protected/scripts/oe-checkout.sh ${BUILD_BRANCH} --no-fix -no-oe
-
-fi
-
-# Ensure .htaccess is set
-if [ ! -f "/openeyes/.htaccess" ]; then
-    echo Renaming .htaccess file
-    mv /openeyes/.htaccess.sample /openeyes/.htaccess
-    sudo chown www-data:www-data /openeyes/.htaccess
-fi
-
-# ensure index is set
-if [ ! -f "/openeyes/index.php" ]; then
-    echo Renaming index.php file
-    mv /openeyes/index.example.php /openeyes/index.php
-    sudo chown www-data:www-data /openeyes/index.php
-fi
-
-/openeyes/protected/scripts/set-profile.sh
-
 # Use docker secret as DB password, or fall back to environment variable
 [ -f /run/secrets/DATABASE_PASS ] && dbpassword="$(</run/secrets/MYSQL_ROOT_PASSWORD)" || dbpassword=${MYSQL_ROOT_PASSWORD:-""}
 [ ! -z $dbpassword ] && dbpassword="-p$dbpassword" || dbpassword="-p''" # Add -p to the beginning of the password (workaround for blank passwords)
 
-echo Testing Database: host=${DATABASE_HOST:-"localhost"} user=${MYSQL_SUPER_USER:-"openeyes"} password="$dbpassword" name=${DATABASE_NAME:-"openeyes"}
+# Test to see if database is accessible. If not we will rebuild it later
+echo Testing Database: host=${DATABASE_HOST:-"localhost"} user=${MYSQL_SUPER_USER:-"openeyes"} name=${DATABASE_NAME:-"openeyes"}
 
-mysql --host=${DATABASE_HOST:-'localhost'} -u $MYSQL_SUPER_USER "$dbpassword" -e 'use '"${DATABASE_NAME:-'openeyes'};"
+db_pre_exist=$( ! mysql --host=${DATABASE_HOST:-'localhost'} -u $MYSQL_SUPER_USER "$dbpassword" -e 'use '"${DATABASE_NAME:-'openeyes'};")$?
 
-if [ $? != "0" ]; then
+# If no web files exist, check them out locally
+if [ ! -d $WROOT/protected ]; then
+  ssh git@github.com -T
+  [ "$?" == "1" ] && cloneroot="git@github.com:" || cloneroot="https://"
+  [ -z "$GIT_ORG" ] && { [ "$cloneroot" == "https://" ] && gitroot="appertafoundation" || gitroot="openeyes";} || gitroot=$GIT_ORG
+  echo cloning "-b ${BUILD_BRANCH} $cloneroot${gitroot}/openeyes.git"
+  git clone --depth=1 -b ${BUILD_BRANCH} $cloneroot${gitroot}/openeyes.git $WROOT
+  # run the standard installer script, do not overwrite database if it already exists
+  $WROOT/protected/scripts/install-oe.sh ${BUILD_BRANCH} --accept ${db_pre_exist/1/--preserve-database}
+  # update db_pre_exist, as it will now exist and we don't want to overwrite it again!
+  db_pre_exist=1
+fi
+
+if [ $db_pre_exist = 0 ]; then
     #If DB doesn't exist then create it - if ENV sample=demo, etc, then call oe-reset (--demo) --no-dependencies --no-migrate
     echo "Database host=${DATABASE_HOST:-'localhost'}; user=${MYSQL_SUPER_USER:-'openeyes'}; name=${DATABASE_NAME:-'openeyes'} was not fount. Creating a new db"
     [ "$USE_DEMO_DATA" = "TRUE" ] && resetparams="--demo" || resetparams=""
-    /openeyes/protected/scripts/oe-reset.sh -b $BUILD_BRANCH $resetparams
+    $WROOT/protected/scripts/oe-reset.sh -b $BUILD_BRANCH $resetparams
 fi
 
-[[ ! -d "/openeyes/node_modules" || ! -d "/openeyes/vendor/yiisoft" ]] && /openeyes/protected/scripts/oe-fix.sh || :
+# Ensure .htaccess is set
+if [ ! -f "$WROOT/.htaccess" ]; then
+    echo Renaming .htaccess file
+    mv $WROOT/.htaccess.sample $WROOT/.htaccess
+    sudo chown www-data:www-data $WROOT/.htaccess
+fi
+
+# ensure index is set
+if [ ! -f "$WROOT/index.php" ]; then
+    echo Renaming index.php file
+    mv $WROOT/index.example.php $WROOT/index.php
+    sudo chown www-data:www-data $WROOT/index.php
+fi
+
+$WROOT/protected/scripts/set-profile.sh
+
+[[ ! -d "$WROOT/node_modules" || ! -d "$WROOT/vendor/yiisoft" ]] && $WROOT/protected/scripts/oe-fix.sh || :
+
+[ -z $(git config --global core.username) ] && git config --global core.username "$GIT_USER" || :
+[ -z $(git config --global core.email) ] && git config --global core.email "$GIT_EMAIL" || :
 
 # Start apache
+echo "Starting apache..."
 apachectl -DFOREGROUND
