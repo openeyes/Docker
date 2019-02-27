35210691496b319123d8958a6e2c1e3e655e0b14
biskyt <biskyt@users.noreply.github.com>
2019-02-27 15:56:33 +0000

    add stict host checking no to ssh config if it is missing

--- web/init.sh
+++ web/init.sh
@@ -40,6 +40,8 @@ idfilecontent="Host github.com\nStrictHostKeyChecking no"
 [ -d /root/.host-ssh ] && rsync -av /root/.host-ssh/ /root/.ssh --exclude known_hosts --delete 2>/dev/null || :
 [ ! -z ${SSH_PRIVATE_KEY} ] && { echo "${SSH_PRIVATE_KEY}" > /tmp/.ssh/id_rsa && echo -e "$idfilecontent\nIdentityFile /tmp/.ssh/id_rsa" > /root/.ssh/config; } || :
 [ -f /run/secrets/SSH_PRIVATE_KEY ] && { cp /run/secrets/SSH_PRIVATE_KEY /tmp/.ssh/id_rsa; echo -e "$idfilecontent\nIdentityFile /tmp/.ssh/id_rsa" > /root/.ssh/config ; } || :
+if ! grep -Fxq "StrictHostKeyChecking no" /root/.ssh/config; then echo -e "$idfilecontent /tmp/.ssh/id_rsa" >> /root/.ssh/config; fi
+true
 [ -d /root/.ssh ] && chmod 600 /root/.ssh/* || :
 [ -d /tmp/.ssh ] && chmod 600 /tmp/.ssh/* || :
 
