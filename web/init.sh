146a8a0d78e6a1cd3fbd563a6f20e01b97a1c2c8
biskyt <biskyt@users.noreply.github.com>
2019-03-15 12:18:04 +0000

    fix typo in if statement

--- web/init.sh
+++ web/init.sh
@@ -114,7 +114,7 @@ if [ "${TRACK_NEW_GIT_COMMITS^^}" == "TRUE" ]; then
   echo "** -= This container automatically pulls from git every 30 minutes =- **"
   echo "************************************************************************"
 
-  [ ! -f /etc/cron.d/track_git ] echo "*/30 * * * * /var/www/openeyes/protected/scripts/oe-update.sh -f >/dev/null 2>&1" > /etc/cron.d/track_git
+  [ ! -f /etc/cron.d/track_git ] && echo "*/30 * * * * /var/www/openeyes/protected/scripts/oe-update.sh -f >/dev/null 2>&1" > /etc/cron.d/track_git | :
 
 fi
 # Start apache
