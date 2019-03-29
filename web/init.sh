727b79ff05852fed688e2a8481512df4fe55ab67
biskyt <biskyt@users.noreply.github.com>
2019-03-29 11:46:42 +0000

    make sure application log exists before trying to tail it

--- web/init.sh
+++ web/init.sh
@@ -145,5 +145,6 @@ echo "*********************************************"
 echo "**       -= END OF STARTUP SCRIPT =-       **"
 echo "*********************************************"
 # Send output of openeyeyes application log to stdout - for viewing with docker logs
+touch $WROOT/protected/runtime/application.log
 tail -n0 $WROOT/protected/runtime/application.log -F | awk '/^==> / {a=substr($0, 5, length-8); next} {print a"App Log:"$0}' &
 apachectl -DFOREGROUND
