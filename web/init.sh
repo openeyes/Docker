c939124b84c80b60255a8aeab7ac6890e1b92254
biskyt <biskyt@users.noreply.github.com>
2019-04-09 16:56:44 +0100

    Fix demo data not installing

--- web/init.sh
+++ web/init.sh
@@ -40,9 +40,9 @@ idfilecontent="Host github.com\nStrictHostKeyChecking no"
 [ -d /tmp/.host-ssh ] && rsync -av /tmp/.host-ssh/ /root/.ssh --exclude known_hosts --delete 2>/dev/null || :
 [ ! -z ${SSH_PRIVATE_KEY} ] && { echo "${SSH_PRIVATE_KEY}" > /tmp/.ssh/id_rsa && echo -e "$idfilecontent\nIdentityFile /tmp/.ssh/id_rsa" > /root/.ssh/config; } || :
 [ -f /run/secrets/SSH_PRIVATE_KEY ] && { echo "USING DOCKER SECRET FOR SSH"; cp /run/secrets/SSH_PRIVATE_KEY /tmp/.ssh/id_rsa; echo -e "$idfilecontent\nIdentityFile /tmp/.ssh/id_rsa" > /root/.ssh/config ; } || :
-if ! grep -Fxq "StrictHostKeyChecking no" /root/.ssh/config; then echo -e "\n$idfilecontent\n" >> /root/.ssh/config; fi
-[ -d /root/.ssh ] && chmod 600 /root/.ssh/* || :
-[ -d /tmp/.ssh ] && chmod 600 /tmp/.ssh/* || :
+if ! grep -Fxq "StrictHostKeyChecking no" /root/.ssh/config 2>/dev/null; then echo -e "\n$idfilecontent\n" >> /root/.ssh/config; fi
+[ `ls -1q /root/.ssh | wc -l` != 0 ] && chmod 600 /root/.ssh/* || :
+[ `ls -1q /tmp/.ssh | wc -l` != 0 ] && chmod -R 600 /tmp/.ssh/* || :
 
 # Use docker secret as DB password, or fall back to environment variable
 [ -f /run/secrets/DATABASE_PASS ] && dbpassword="$(</run/secrets/MYSQL_ROOT_PASSWORD)" || dbpassword=${MYSQL_ROOT_PASSWORD:-""}
@@ -59,30 +59,38 @@ echo Testing Database: host=${DATABASE_HOST:-"localhost"} user=${MYSQL_SUPER_USE
 # NOTE: The $? on the end of the next line is very important - it converts the output to a 1 or 0
 db_pre_exist=$( ! mysql --host=${DATABASE_HOST:-'localhost'} -u $MYSQL_SUPER_USER "$dbpassword" -e "use ${DATABASE_NAME:-'openeyes'};" 2>/dev/null)$?
 
-[ db_pre_exist == "1" ] && echo "...database ${DATABASE_NAME:-'openeyes'} found." || echo "...could not find database ${DATABASE_NAME:-'openeyes'}. It will be (re) created..."
+[ "$db_pre_exist" = "1" ] && echo "...database ${DATABASE_NAME:-'openeyes'} found." || echo "...could not find database ${DATABASE_NAME:-'openeyes'}. It will be (re) created..."
+
+newinstall=0
 
 # If no web files exist, check them out locally
-if [ ! -d $WROOT/protected/modules/eyedraw/src ]; then
+if [ ! -f $WROOT/protected/config/core/common.php ]; then
   ssh git@github.com -T
   [ "$?" == "1" ] && cloneroot="git@github.com:" || cloneroot="https://github.com/"
   # If GIT_ORG is not specified then - If using https we defualt to appertafoundation. If ussing ssh we default to openeyes
   [ -z "$GIT_ORG" ] && { [ "$cloneroot" == "https://github.com/" ] && gitroot="appertafoundation" || gitroot="openeyes";} || gitroot=$GIT_ORG
 
   # If openeyes files don't already exist then clone them
-  [ ! -f $WROOT/protected/runtime/testme ] && { echo cloning "-b ${BUILD_BRANCH} $cloneroot${gitroot}/openeyes.git"; git clone -b ${BUILD_BRANCH} $cloneroot${gitroot}/openeyes.git $WROOT; } || :
+  echo cloning "-b ${BUILD_BRANCH} $cloneroot${gitroot}/openeyes.git"
+  git clone -b ${BUILD_BRANCH} $cloneroot${gitroot}/openeyes.git $WROOT
 
-  # run the standard installer script, do not overwrite database if it already exists
-  initparams="--accept"
-  [ "$db_pre_exist" == "1" ] && initparams="$initparams --preserve-database" || :
-  $WROOT/protected/scripts/install-oe.sh ${BUILD_BRANCH} --accept $initparams
-  # update db_pre_exist, as it will now exist and we don't want to overwrite it again!
-  db_pre_exist=1
-  echo "true" > /initialised.oe
+  newinstall=1
 fi
 
-$WROOT/protected/scripts/set-profile.sh
-echo "source /etc/profile.d/git-branch.sh" > ~/.bash_profile
-source /etc/profile.d/git-branch.sh
+# If this is a new container (using existing git files), then we need to initialise the config
+initparams="--accept --no-migrate --preserve-database --no-sample"
+[[ $newinstall = 0 && -d "$WROOT/protected/modules/eyedraw/src" ]] && initparams="$initparams --no-checkout" || :
+#[ "$db_pre_exist" != "1" ] && initparams="$initparams --no-fix" || :
+[ ! -f /initialised.oe ] && { echo "Initialising new container..."; $WROOT/protected/scripts/install-oe.sh $initparams && echo "true" > /initialised.oe; } || :
+
+if [ "$db_pre_exist" != "1" ]; then
+    #If DB doesn't exist then create it - if ENV sample=demo, etc, then call oe-reset (--demo) --no-dependencies --no-migrate
+    echo "Database host=${DATABASE_HOST:-'localhost'}; user=${MYSQL_SUPER_USER:-'openeyes'}; name=${DATABASE_NAME:-'openeyes'} was not fount. Creating a new db"
+    [ "$USE_DEMO_DATA" = "TRUE" ] && resetparams="--demo" || resetparams=""
+    $WROOT/protected/scripts/oe-reset.sh -b $BUILD_BRANCH $resetparams
+fi
+
+[[ ! -d "$WROOT/node_modules" || ! -d "$WROOT/vendor/yiisoft" ]] && { echo -e "\n\nDependencies not found, installing now...\n\n"; $WROOT/protected/scripts/oe-fix.sh; } || :
 
 # Ensure .htaccess is set
 if [ ! -f "$WROOT/.htaccess" ]; then
@@ -98,20 +106,6 @@ if [ ! -f "$WROOT/index.php" ]; then
     sudo chown www-data:www-data $WROOT/index.php
 fi
 
-# If this is a new container (using existing git files), then we need to initialise the config
-initparams="--no-checkout --accept --no-migrate"
-[ "$db_pre_exist" == "1" ] && initparams="$initparams --preserve-database" || :
-[ ! -f /initialised.oe ] && { $WROOT/protected/scripts/install-oe.sh $initparams  && echo "true" > /initialised.oe && db_pre_exist=1; } || :
-
-if [ $db_pre_exist = 0 ]; then
-    #If DB doesn't exist then create it - if ENV sample=demo, etc, then call oe-reset (--demo) --no-dependencies --no-migrate
-    echo "Database host=${DATABASE_HOST:-'localhost'}; user=${MYSQL_SUPER_USER:-'openeyes'}; name=${DATABASE_NAME:-'openeyes'} was not fount. Creating a new db"
-    [ "$USE_DEMO_DATA" = "TRUE" ] && resetparams="--demo" || resetparams=""
-    $WROOT/protected/scripts/oe-reset.sh -b $BUILD_BRANCH $resetparams
-fi
-
-[[ ! -d "$WROOT/node_modules" || ! -d "$WROOT/vendor/yiisoft" ]] && $WROOT/protected/scripts/oe-fix.sh || :
-
 
 [[ -z $(git config --global user.name)  && ! -z $GIT_USER ]] && { git config --global user.name "$GIT_USER" && echo "git global user set to $GIT_USER"; } || :
 [[ -z $(git config --global user.email) && ! -z $GIT_EMAIL ]] && { git config --global user.email "$GIT_EMAIL" && echo "git global email set to $GIT_EMAIL"; } || :
@@ -126,16 +120,10 @@ if [ "${TRACK_NEW_GIT_COMMITS^^}" == "TRUE" ]; then
 
 fi
 
-# set subfolder as alias if HOST_SUBFOLDER env is set
-# i.e. make site accessible on http://localhost/$HOST_SUBFOLDER
-if [ ! -z $HOST_SUBFOLDER ]; then
-  echo "listinging on http://localhost/$HOST_SUBFOLDER/"
-  # only modify the virtual host once, as we don't want to keep writing to non-persistent storage
-  [ ! -f /aliasset.oe ] && { sed -i "s|.*Alias SubFolder.*|Alias /${HOST_SUBFOLDER} $WROOT|" /etc/apache2/sites-available/000-default.conf && echo "set to $HOST_SUBFOLDER" > /aliasset.oe; } || :
-  # Reset the .htaccess on each boot, as this should be in persistent storage
-  sed -i "s|.*RewriteBase.*|RewriteBase /${HOST_SUBFOLDER}/|" $WROOT/.htaccess
 
-fi
+$WROOT/protected/scripts/set-profile.sh
+echo "source /etc/profile.d/git-branch.sh" > ~/.bash_profile
+source /etc/profile.d/git-branch.sh
 
 # Start apache
 echo "Starting opeyes apache process..."
